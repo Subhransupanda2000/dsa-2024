cf8b183559ec72f7afd84c81aec5748b595fb76d
Subhransupanda2000 <imsubhransupanda2000@gmail.com>
2024-03-14 17:30:32 +0530

    two test cases added

--- src/test/java/com/example/array/TestSecondLargestElement.java
+++ src/test/java/com/example/array/TestSecondLargestElement.java
@@ -2,6 +2,7 @@ package com.example.array;
 
 import static junit.framework.TestCase.assertEquals;
 
+import junit.framework.AssertionFailedError;
 import org.junit.Test;
 
 public class TestSecondLargestElement {
@@ -10,6 +11,19 @@ public class TestSecondLargestElement {
         int arr[] = {1, 6, 3, 9};
         int expected = 6;
         int actual = SecondLargestElement.secondLargest(arr);
+        assertEquals(expected, actual);
+    }
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void testWithNullArray() {
+        int[] arr = {};
+        SecondLargestElement.secondLargest(arr);
+    }
+
+    @Test(expected = AssertionFailedError.class)
+    public void testWithEqualElements() {
+        int arr[] = {2, 2, 2, 2, 2};
+        int actual = SecondLargestElement.secondLargest(arr);
+        int expected = 2;
         assertEquals(expected, actual);
     }
 }
